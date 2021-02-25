261172483b61862d7ff02638f48595f284624344
Tim Tan <timmytan18@gmail.com>
2021-02-25 13:44:15 -0500

    Change timicon instead of gatsby-icon

--- public/render-page.js
+++ public/render-page.js
@@ -129,7 +129,7 @@ var plugins = [{
     "background_color": "#663399",
     "theme_color": "#663399",
     "display": "minimal-ui",
-    "icon": "src/images/gatsby-icon.png",
+    "icon": "src/images/timicon.png",
     "legacy": true,
     "theme_color_in_head": true,
     "cache_busting_mode": "query",
