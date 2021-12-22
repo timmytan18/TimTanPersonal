449385b0946c42ab7681d8ee4bf91d4754d85c99
Tim Tan <timmytan18@gmail.com>
2021-12-22 15:48:07 -0500

    Create redirect page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,16 +1,46 @@
-import React from "react"
+import React, { useEffect } from "react"
+import Fade from "react-reveal/Fade"
+import timImg from '../images/timanimoji.gif'
 
 const BEACONS_LINK = "https://beacons.ai/timtan";
+const REDIRECT = "Visit Links";
 
 const Beacons = () => {
-  if (typeof window !== 'undefined') {
-    const win = window.open(BEACONS_LINK, '_self');
-    if (win != null) {
-      win.focus();
+
+  useEffect(() => {
+    //preload image
+    const img = new Image();
+    img.src = timImg;
+
+    if (typeof window !== 'undefined') {
+      const win = window.open(BEACONS_LINK, '_self');
+      if (win != null) {
+        win.focus();
+      }
     }
-  }
+  }, []);
+
   return (
-    <></>
+    <div className="section" id="home">
+			<div className="container">
+				<div className="header-wrapper">
+					<Fade bottom>
+						<h2>
+							Hi there!{" "}
+							<span role="img" aria-label="Emoji">
+								👋
+              </span>
+						</h2>
+            <div className="animoji-container">
+              <img src={timImg} alt="loading..." />
+            </div>
+					</Fade>
+					<Fade bottom>
+						<p><a href={BEACONS_LINK}><strong>{REDIRECT}</strong></a> ➡️</p>
+					</Fade>
+				</div>
+			</div>
+		</div>
   );
 }
 
