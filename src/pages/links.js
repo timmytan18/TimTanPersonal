ede3af2230ca49f845f36bd46c18e17ede6fd3e1
Tim Tan <timmytan18@gmail.com>
2021-12-22 15:57:40 -0500

    Remove image from redirect page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react"
-import Fade from "react-reveal/Fade"
+import React from "react"
 import timImg from '../images/timanimoji.gif'
 
 const BEACONS_LINK = "https://beacons.ai/timtan";
@@ -7,40 +6,27 @@ const REDIRECT = "Visit Links";
 
 const Beacons = () => {
 
-  useEffect(() => {
-    //preload image
-    const img = new Image();
-    img.src = timImg;
-
-    if (typeof window !== 'undefined') {
-      const win = window.open(BEACONS_LINK, '_self');
-      if (win != null) {
-        win.focus();
-      }
+  if (typeof window !== 'undefined') {
+    const win = window.open(BEACONS_LINK, '_self');
+    if (win != null) {
+      win.focus();
     }
-  }, []);
+  }
 
   return (
     <div className="section" id="home">
-			<div className="container">
-				<div className="header-wrapper">
-					<Fade bottom>
-						<h2>
-							Hi there!{" "}
-							<span role="img" aria-label="Emoji">
-								👋
-              </span>
-						</h2>
-            <div className="animoji-container">
-              <img src={timImg} alt="loading..." />
-            </div>
-					</Fade>
-					<Fade bottom>
-						<p><a href={BEACONS_LINK}><strong>{REDIRECT}</strong></a> ➡️</p>
-					</Fade>
-				</div>
-			</div>
-		</div>
+      <div className="container">
+        <div className="header-wrapper">
+          <h2>
+            Hi there!{" "}
+            <span role="img" aria-label="Emoji">
+              👋
+            </span>
+          </h2>
+          <p><a href={BEACONS_LINK}><strong>{REDIRECT}</strong></a> ➡️</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
